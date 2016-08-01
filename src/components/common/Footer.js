28e4b9773af2b9f7e5693faf7f02fde48194b0e2
blurbyte <info@blurbyte.com>
2016-08-01 12:04:42 +0200

    Size of main footer reduced

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => (
   <footer className="main-footer">
     <div className="wrapper">
       <div className="social-links">
-        <a href="https://twitter.com/" target="_blank"><TwitterIcon /></a>
-        <a href="https://github.com/blurbyte" target="_blank"><GithubIcon /></a>
+        <a href="https://twitter.com/" target="_blank"><TwitterIcon width={40} height={40} /></a>
+        <a href="https://github.com/blurbyte" target="_blank"><GithubIcon width={40} height={40} /></a>
       </div>
       <p className="contact-info">contact<span className="email">info@blurbyte.com</span></p>
       </div>
