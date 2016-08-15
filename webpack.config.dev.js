670242022223fa5d728881889fa66506d8f6e71a
blurbyte <info@blurbyte.com>
2016-08-15 02:16:16 +0200

    Webpack prod build cache busting integrated

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,5 @@
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import autoprefixer from 'autoprefixer';
 
 const GLOBALS = {
@@ -9,31 +10,37 @@ const GLOBALS = {
 export default {
   debug: true,
   devtool: 'cheap-module-eval-source-map',
-  noInfo: false,
+  noInfo: true,
   entry: [
+    './src/webpack-public-path',
     'webpack-hot-middleware/client?reload=true',
     'whatwg-fetch',
-    './src/index.js'
+    './src/index'
   ],
   target: 'web',
   output: {
     path: __dirname + '/dist',
-    publicPath: 'http://localhost:3000/',
+    publicPath: '/',
     filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: './src'
   },
   plugins: [
     new webpack.DefinePlugin(GLOBALS),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.ejs',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      },
+      inject: true
+    })
   ],
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
-      {test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=assets/[name].[ext]'},
-      {test: /\.[ot]tf$/, loader: 'file-loader?limit=10000&mimetype=application/octet-stream&name=assets/[name].[ext]'},
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+      { test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=assets/[name].[ext]' },
+      { test: /\.[ot]tf$/, loader: 'file-loader?limit=10000&mimetype=application/octet-stream&name=assets/[name].[ext]' },
       { test: /(\.css)$/, loaders: ['style', 'css?sourceMap', 'postcss'] }
     ]
   },
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,7 @@
 import webpack from 'webpack';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import autoprefixer from 'autoprefixer';
 
 const GLOBALS = {
@@ -10,32 +12,44 @@ const GLOBALS = {
 export default {
   debug: true,
   devtool: 'source-map',
-  noInfo: false,
+  noInfo: true,
   entry: [
     'whatwg-fetch',
-    './src/index.js'
-    ],
+    './src/index'
+  ],
   target: 'web',
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: './dist'
+    filename: 'bundle-[chunkhash].js'
   },
   plugins: [
+    new WebpackMd5Hash(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin(GLOBALS),
-    new ExtractTextPlugin('styles.css'),
+    new ExtractTextPlugin('styles-[contenthash].css'),
+    new HtmlWebpackPlugin({
+      template: 'src/index.ejs',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true
+    }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
-      {test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=assets/[name].[ext]'},
-      {test: /\.[ot]tf$/, loader: 'file-loader?limit=10000&mimetype=application/octet-stream&name=assets/[name].[ext]'},
+      { test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=assets/[name].[ext]' },
+      { test: /\.[ot]tf$/, loader: 'file-loader?limit=10000&mimetype=application/octet-stream&name=assets/[name].[ext]' },
       { test: /(\.css)$/, loader: ExtractTextPlugin.extract('css?sourceMap!postcss') }
     ]
   },
