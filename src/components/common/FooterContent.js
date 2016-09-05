af26bdc78e8e071231ed47cf7b94b2c2d2e1a005
blurbyte <info@blurbyte.com>
2016-09-05 15:46:44 +0200

    Social links updated

--- src/components/common/FooterContent.js
+++ src/components/common/FooterContent.js
@@ -8,7 +8,7 @@ const FooterContent = ({expanded}) => (
     <footer className={`main-footer${expanded ? ' expanded': ''}`}>
       <div className="main-footer-info">
         <div className="social-links">
-          <a href="https://twitter.com/" target="_blank"><TwitterIcon width={40} height={40} /></a>
+          <a href="https://twitter.com/blurbyte" target="_blank"><TwitterIcon width={40} height={40} /></a>
           <a href="https://github.com/blurbyte" target="_blank"><GithubIcon width={40} height={40} /></a>
         </div>
         <p className="contact-info">contact<span className="email">info@blurbyte.com</span></p>
